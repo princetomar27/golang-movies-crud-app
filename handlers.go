19f288b3584f49922b17fa1115d415f1a3c835c5
Prince Tomar <prince@farmsetu.co>
2024-11-17 10:39:36 +0530

    Get movie from query parameter instead of path params

--- handlers.go
+++ handlers.go
@@ -3,21 +3,25 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"strings"
 )
 
 func GetMovie2(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type","application/json")
 
-	movieId := strings.TrimPrefix(r.URL.Path,"/movie/")
+	// movieId := strings.TrimPrefix(r.URL.Path,"/movie/")
 
- 
+	movieIdFromQP := r.URL.Query().Get("id")
+
+	if movieIdFromQP == ""{
+		http.Error(w, "Movie ID missing in QP", http.StatusBadRequest)
+		return
+	}
 
 	// params := mux.Vars(r)
 	// movieId := params["id"]
 
 	for _, currentMovie := range Movies{
-		if currentMovie.ID == movieId{
+		if currentMovie.ID == movieIdFromQP{
 			json.NewEncoder(w).Encode(currentMovie)
 			return
 		}
